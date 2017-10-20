746c5580b1f362cb693ef2cc4f05b13c6282ff0e
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-20 19:09:57 +0200

    conexion de prueba con la base de datos para ver si funciona

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 // cabecera de la pagina
 include "includes/header.php";
-?>
 
-<p>Texto del archivo 'index.php'</p>
+require_once 'includes/DB/Conexion.php';
 
-<?php
+$con = new Conexion();
+
+$res = $con->query('select * from usuario');
+
+echo "<p>Contenido de index.php: </p>";
+echo "<p>Probando a listar usuarios<p>";
+while ($row = $res->fetch_object()) {
+    echo $row->dni . " " . $row->nombre . " " . $row->apellidos . "<br>";
+}
+
 // pie de pagina
 include "includes/footer.php";
 ?>
