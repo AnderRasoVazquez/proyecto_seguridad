1e1defdaf8dc37b83bed7747a75a701d6cbec244
DavidPerezGomez <davidperezgo@gmail.com>
2017-10-22 22:56:43 +0200

    Convertido formulario_login.php a html con funciones en php.

--- formulario_login.php
+++ formulario_login.php
@@ -1,30 +1,30 @@
 <?php
-
 // quería trastear y probar a poner una imagen
 $image = "img/ragnaros.jpg";
-echo("<html>");
-    echo("<title>");
-        echo("identify INSECT!");
-    echo("</title>");
-
-    echo("<body>");
-        // empieza el formulario
-        echo("<form>");
-            /*
-            se comprueba si el archivo de la imagen existe
-            si no existe no se hace nada
-            */
-            if (file_exists($image)) {
-                echo("<image src=$image title='TOO SOON!'>");
-            }
-            // este es el equivalente a meter enters para alinear párrafor fml
-            echo("<br><br>");
-            echo("Nombre: <input type='text' name='name' ><br>");
-            echo("Contraseña: <input type='password' name='pass'><br>");
-            echo("<input type='button' value='Enviar'>");
-            echo("<input type='reset' value='Borrar'>");
-        echo("</form>");
-    echo("</body>");
-echo("</html>");
-
 ?>
+
+<html>
+    <head>
+        <title>
+            identify INSETC!
+        </title>
+    </head>
+    <body>
+        <form>
+            <?php
+                /*
+                se comprueba si el archivo de la imagen existe
+                si no existe no se hace nada
+                */
+                if (file_exists($image)) {
+                    echo("<image src=$image title='TOO SOON!'>");
+                }
+            ?>
+            <br><br>
+            Nombre: <input type="text" name="name"><br>
+            Contraseña: <input type="text" name="pass"><br>
+            <input type='button' value='Enviar'>
+            <input type='reset' value='Borrar'>
+        </form>
+    </body>
+</html>
