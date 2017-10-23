4a142ce8153cf99b8a17a52d5edb53ed0237c1d0
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-23 09:23:28 +0200

    indentación arreglada

--- formulario_post.php
+++ formulario_post.php
@@ -5,19 +5,18 @@ include "includes/header.php";
 
 <form action="add_post.php" method="post" id="formulario_post">
     <div id="textinput">
-    <legend>Título:</legend>
-    <input type="text" name="titulo" value="">
-    <legend>Autor:</legend>
-    <input type="text" name="autor" value="">
-    <legend>Categorías:</legend>
-    <input type="text" name="categorias[]" value="">
-    <div id="tag_container">
-    </div>
-    <a href="#" onclick="add_tag()">+Add tag</a>
-        <!-- <h3>Texto en Markdown</h3> -->
+        <legend>Título:</legend>
+        <input type="text" name="titulo" value="">
+        <legend>Autor:</legend>
+        <input type="text" name="autor" value="">
+        <legend>Categorías:</legend>
+        <input type="text" name="categorias[]" value="">
+        <div id="tag_container">
+        </div>
+        <a href="#" onclick="add_tag()">+Add tag</a>
         <legend>Texto en Markdown:</legend>
         <textarea id="sourceTA" oninput="run()" form="formulario_post"></textarea>
-    <input type="button" name="submit" value="Crear snippet">
+        <input type="button" name="submit" value="Crear snippet">
     </div>
     <div id="prev_panel">
         <legend>Previsualización</legend>
@@ -30,14 +29,14 @@ include "includes/header.php";
 include "includes/footer.php";
 ?>
 <script type="text/javascript">
-    function add_tag() {
-        var container = document.getElementById("tag_container")
-        // Create an <input> element, set its type and name attributes
-        var input = document.createElement("input");
-        input.type = "text";
-        input.name = "categorias[]";
-        container.appendChild(input);
-        // Append a line break
-        container.appendChild(document.createElement("br"));
-    }
-    </script>
+function add_tag() {
+    var container = document.getElementById("tag_container")
+    // Create an <input> element, set its type and name attributes
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = "categorias[]";
+    container.appendChild(input);
+    // Append a line break
+    container.appendChild(document.createElement("br"));
+}
+</script>
