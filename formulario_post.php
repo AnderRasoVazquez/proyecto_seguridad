b8eadba12cecc41ea5059cc9b18a9732ebb6c164
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-23 09:22:27 +0200

    creado el contenido de formulario post

--- formulario_post.php
+++ formulario_post.php
@@ -3,20 +3,41 @@
 include "includes/header.php";
 ?>
 
-<form action="add_post.php" method="post">
+<form action="add_post.php" method="post" id="formulario_post">
     <div id="textinput">
-        <h3>Texto en Markdown</h3>
-        <textarea id="sourceTA" oninput="run()"></textarea>
+    <legend>Título:</legend>
+    <input type="text" name="titulo" value="">
+    <legend>Autor:</legend>
+    <input type="text" name="autor" value="">
+    <legend>Categorías:</legend>
+    <input type="text" name="categorias[]" value="">
+    <div id="tag_container">
     </div>
-    <div id="right">
-        <h3>Previsualización en vivo</h3>
+    <a href="#" onclick="add_tag()">+Add tag</a>
+        <!-- <h3>Texto en Markdown</h3> -->
+        <legend>Texto en Markdown:</legend>
+        <textarea id="sourceTA" oninput="run()" form="formulario_post"></textarea>
+    <input type="button" name="submit" value="Crear snippet">
+    </div>
+    <div id="prev_panel">
+        <legend>Previsualización</legend>
         <div id="targetDiv"></div>
     </div>
-    <input type='button' value='Enviar'>
-
 </form>
 
 <?php
 // pie de pagina
 include "includes/footer.php";
 ?>
+<script type="text/javascript">
+    function add_tag() {
+        var container = document.getElementById("tag_container")
+        // Create an <input> element, set its type and name attributes
+        var input = document.createElement("input");
+        input.type = "text";
+        input.name = "categorias[]";
+        container.appendChild(input);
+        // Append a line break
+        container.appendChild(document.createElement("br"));
+    }
+    </script>
