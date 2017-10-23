0c03eda380a624f622b42814990a102167b331a9
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-23 19:01:16 +0200

    añadidas referencias

--- formulario_post.php
+++ formulario_post.php
@@ -17,6 +17,13 @@ include "includes/header.php";
         <a href="#" onclick="rm_tag()">-Revome tag</a>
         <legend>Texto en Markdown:</legend>
         <textarea id="sourceTA" class="form-control" name="content" oninput="convert_markdown()" form="formulario_post"></textarea>
+        <legend>References:</legend>
+        <input class="form-control" type="text" id="reference" name="references[]" value="">
+        <div id="reference_container">
+        </div>
+        <a href="#" onclick="add_reference()">+Add reference</a>
+        <a href="#" onclick="rm_reference()">-Revome reference</a>
+        <br>
         <input type="button" class="btn btn-success" name="submit" onclick="checkPost()" value="+Add Snippet">
     </div>
 </form>
@@ -43,6 +50,24 @@ function add_tag() {
 
 function rm_tag() {
     var container = document.getElementById("tag_container")
+    // Remove the container's last child twice:
+    // First the input
+    container.removeChild(container.lastChild);
+}
+
+function add_reference() {
+    var container = document.getElementById("reference_container")
+    // Create an <input> element, set its type and name attributes
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = "references[]";
+    input.className += " form-control";
+    container.appendChild(input);
+    // Append a line break
+}
+
+function rm_reference() {
+    var container = document.getElementById("reference_container")
     // Remove the container's last child twice:
     // First the input
     container.removeChild(container.lastChild);
