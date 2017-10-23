23f58938e1c809252c1d4c0773034298fc0d3095
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-23 08:32:22 +0200

    añadidas etiquetas básicas html y las librerias para renderizar markdown

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,6 @@
-<p>Texto del archivo 'includes/footer.php'</p>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/showdown/1.7.6/showdown.js"></script>
+    <script src="script.js">
+    </script>
+</body>
+
+</html>
