373144d1defa613ae1bc3f2e1306b8699dc23aae
DavidPerezGomez <davidperezgo@gmail.com>
2017-10-24 13:46:54 +0200

    convertido el campo de contraseña a tipo contraseña

--- formulario_login.php
+++ formulario_login.php
@@ -8,7 +8,7 @@ include "includes/header.php";
         <legend>Nombre:</legend>
         <input class="form-control" type="text" name="name">
         <legend>Contraseña:</legend>
-        <input class="form-control" type="text" name="pass">
+        <input class="form-control" type="password" name="pass">
         <!-- párrafo en blanco para añadir posible mensaje de error -->
         <p id="error"></p>
         <input type='button' onclick="checkLogin()" value='Login'>
