8da3777857745d6eedab49db053493b8db0a02df
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-24 13:20:00 +0200

    añadida busqueda

--- includes/navigation_bar.php
+++ includes/navigation_bar.php
@@ -11,6 +11,10 @@
                     <li><a href="formulario_registro.php">Sign up</a></li>
                     <li><a href="formulario_post.php">Submit Snippet!</a></li>
                 </ul>
+                <form id="nav_search" class="form-inline">
+                    <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                    <button  class="btn btn-info" type="submit">Search</button>
+                </form>
             </nav>
         </div>
     </header>
