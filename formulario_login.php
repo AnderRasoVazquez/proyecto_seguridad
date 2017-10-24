f8cf95ff87a0e63e049b4c4f04f0b844b27e2de1
DavidPerezGomez <davidperezgo@gmail.com>
2017-10-24 12:11:42 +0200

    eliminado enlace y arreglado id en formulario_login.php

--- formulario_login.php
+++ formulario_login.php
@@ -3,8 +3,8 @@
 // cabecera de la pagina
 include "includes/header.php";
 ?>
-    <h1>Log in to <a href="index.php">Snippedia</a></h1>
-    <form action="index.html" method="post" id="login_post">
+    <h1>Log in to Snippedia</h1>
+    <form action="index.html" method="post" id="loginPost">
         <legend>Nombre:</legend>
         <input class="form-control" type="text" name="name">
         <legend>Contraseña:</legend>
