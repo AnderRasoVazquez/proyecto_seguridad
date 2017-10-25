679625f578a03fdfc4817736364419a1ad297d0b
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-25 18:13:41 +0200

    codigo javascript sacado de la página y metido a script.js

--- formulario_post.php
+++ formulario_post.php
@@ -14,16 +14,16 @@ include "includes/header.php";
         <input class="form-control" type="text" id="tag" name="tags[]" value="">
         <div id="tag_container">
         </div>
-        <a href="#" onclick="add_tag()">+Add tag</a>
-        <a href="#" onclick="rm_tag()">-Revome tag</a>
+        <input type="button" class="btn btn-sm btn-dark" onclick="add_tag()" value="+ Tag">
+        <input type="button" class="btn btn-sm btn-dark" onclick="rm_tag()" value="- Tag">
         <legend>Text in Markdown format:</legend>
         <textarea id="sourceTA" class="form-control" name="content" oninput="convert_markdown()" form="formulario_post"></textarea>
         <legend>References:</legend>
         <input class="form-control" type="text" id="reference" name="references[]" value="">
         <div id="reference_container">
         </div>
-        <a href="#" onclick="add_reference()">+Add reference</a>
-        <a href="#" onclick="rm_reference()">-Revome reference</a>
+        <input type="button" class="btn btn-sm btn-dark" onclick="add_reference()" value="+ Reference">
+        <input type="button" class="btn btn-sm btn-dark" onclick="rm_reference()" value="- Reference">
         <br>
         <br>
         <input type="button" class="btn btn-success" name="submit" onclick="checkPost()" value="+Add Snippet">
@@ -38,36 +38,3 @@ include "includes/header.php";
 // pie de pagina
 include "includes/footer.php";
 ?>
-<script type="text/javascript">
-function add_tag() {
-    var container = document.getElementById("tag_container")
-    // Create an <input> element, set its type and name attributes
-    var input = document.createElement("input");
-    input.type = "text";
-    input.name = "categorias[]";
-    input.className += " form-control";
-    container.appendChild(input);
-}
-
-function rm_tag() {
-    var container = document.getElementById("tag_container")
-    // Remove the container's last child
-    container.removeChild(container.lastChild);
-}
-
-function add_reference() {
-    var container = document.getElementById("reference_container")
-    // Create an <input> element, set its type and name attributes
-    var input = document.createElement("input");
-    input.type = "text";
-    input.name = "references[]";
-    input.className += " form-control";
-    container.appendChild(input);
-}
-
-function rm_reference() {
-    var container = document.getElementById("reference_container")
-    // Remove the container's last child
-    container.removeChild(container.lastChild);
-}
-</script>
--- js/script.js
+++ js/script.js
@@ -18,40 +18,39 @@ function convert_markdown() {
     }
 }
 
-function checkSignup(){
+function checkSignup() {
 
 }
 
-function checkLogin(){
+function checkLogin() {
     var name = document.forms["loginForm"]["name"].value;
     var pass = document.forms["loginForm"]["pass"].value;
     // comprobar si los datos de los campos son válidos
     if (isNameCorrect(name) && isPassCorrect(pass)) {
         console.log("true");
         document.getElementById("loginForm").submit();
-    }
-    else {
+    } else {
         console.log("false");
         document.getElementById("error").innerHTML =
-        "El nombre de usuario o contraseña no es válido."
+            "El nombre de usuario o contraseña no es válido."
     }
 }
 
-function isNameCorrect(pName){
+function isNameCorrect(pName) {
     var success = false;
     // condiciones para que el nombre de usuario sea válido
     success = pName != "";
     return success;
 }
 
-function isPassCorrect(pPass){
+function isPassCorrect(pPass) {
     var success = false;
     // condiciones para que la contraseña sea válida
     success = pPass != "";
     return success;
 }
 
-function checkPost(){
+function checkPost() {
     var title = document.forms["form_post"]["title"].value;
     var author = document.forms["form_post"]["author"].value;
     var content = document.getElementById("sourceTA").value;
@@ -66,6 +65,38 @@ function checkPost(){
     }
 }
 
-function checkEdit(){
+function checkEdit() {
 
+}
+
+function add_tag() {
+    var container = document.getElementById("tag_container")
+    // Create an <input> element, set its type and name attributes
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = "categorias[]";
+    input.className += " form-control";
+    container.appendChild(input);
+}
+
+function rm_tag() {
+    var container = document.getElementById("tag_container")
+    // Remove the container's last child
+    container.removeChild(container.lastChild);
+}
+
+function add_reference() {
+    var container = document.getElementById("reference_container")
+    // Create an <input> element, set its type and name attributes
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = "references[]";
+    input.className += " form-control";
+    container.appendChild(input);
+}
+
+function rm_reference() {
+    var container = document.getElementById("reference_container")
+    // Remove the container's last child
+    container.removeChild(container.lastChild);
 }
