7cf595f4ad6814d2ea3fee8a34f93cd884c55a36
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-25 17:48:50 +0200

    salto de línea quitado

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
     </div> <!-- div#content -->
     <script src="http://cdnjs.cloudflare.com/ajax/libs/showdown/1.7.6/showdown.js"></script>
-    <script src="js/script.js">
-    </script>
+    <script src="js/script.js"> </script>
 </body>
 
 </html>
