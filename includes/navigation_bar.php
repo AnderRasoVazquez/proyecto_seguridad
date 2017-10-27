2c29d32deb52d2accc581901ca51200e0d22cf3d
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-27 11:11:44 +0200

    logo cambiado a trozo de papel

--- includes/navigation_bar.php
+++ includes/navigation_bar.php
@@ -6,7 +6,7 @@
             </a> -->
             <nav>
                 <ul class="nav_ul">
-                    <a href="index.php"><li class="no-padding logo"><i class="material-icons logo-icon">code</i>Snippedia</li></a>
+                    <a href="index.php"><li class="no-padding logo"><i class="material-icons logo-icon">receipt</i>Snippedia</li></a>
                     <a href="formulario_login.php"><li>Login</li></a>
                     <a href="formulario_registro.php"><li>Sign up</li></a>
                     <a href="formulario_post.php"><li>Submit Snippet!</li></a>
