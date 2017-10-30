cb6c867e579ad915a0f2669e798daf2a48cb651d
DavidPerezGomez <davidperezgo@gmail.com>
2017-10-30 13:24:29 +0100

    la comprobaciones del login se realizan con el has en lugar de con la contraseña

--- login.php
+++ login.php
@@ -7,30 +7,29 @@ require_once 'includes/DB/Conexion.php';
 $conn = new Conexion();
 $dni = $_POST["dni"];
 $pass = $_POST["pass"];
-$res = $conn->query("SELECT * FROM usuario WHERE dni='$dni' AND contraseña='$pass'");
+$res = $conn->query("SELECT * FROM usuario WHERE dni='$dni'");
 
-if ($res == false) {
-    //caso de error
-    exit();
-} elseif (mysqli_num_rows($res)!=0) {
-    // resultado válido
-    // se inicia la sesión
-    session_start();
-    // variables de sesión
-    $_SESSION["currentUser"] = $dni;
-    $_SESSION["currentUserName"] = $res->fetch_object()->nombre;
-    // redirige a la página principal
-    header("Location: index.php");
-    exit();
-} else {
-    // resultado no válido
-    // devuelve a la página de login
-    header("Location: formulario_login.php");
-    showLoginError();
-    exit();
+if (mysqli_num_rows($res)!=0) {
+    // usuario encontrado
+    if (password_verify($pass, $res->fetch_object()->hash)) {
+        // contraseña verificada
+        // se inicia la sesión
+        session_start();
+        // variables de sesión
+        $_SESSION["currentUser"] = $dni;
+        $_SESSION["currentUserName"] = $res->fetch_object()->nombre;
+        // redirige a la página principal
+        $conn->close();
+        header("Location: index.php");
+        exit();
+    }
 }
-
+// resultado no válido
+// devuelve a la página de login
 $conn->close();
+header("Location: formulario_login.php");
+exit();
+
 
 // cabecera de la pagina
 include "includes/footer.php";
