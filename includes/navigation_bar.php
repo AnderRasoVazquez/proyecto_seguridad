2ba9b298f5de1d115d233aafd41e265b2afd893a
Ander Raso Vázquez <anderraso@gmail.com>
2017-10-30 21:56:15 +0100

    search implementado

--- includes/navigation_bar.php
+++ includes/navigation_bar.php
@@ -22,9 +22,9 @@
                     <a href="latest_snippets.php"><li>Latest Snippets</li></a>
                     <a href="formulario_post.php"><li>Submit Snippet!</li></a>
                 </ul>
-                <form id="nav_search" class="form-inline">
-                    <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-                    <a  class="btn btn-info" type="submit"><i class="material-icons">search</i></a>
+                <form id="nav_search" action="search_snippet.php" method="post" class="form-inline">
+                    <input class="form-control" type="search" placeholder="Search" name="search_term" aria-label="Search">
+                    <a class="btn btn-info" onclick="document.getElementById('nav_search').submit()"><i class="material-icons">search</i></a>
                 </form>
             </nav>
         </div>
---
+++ search_snippet.php
@@ -0,0 +1,27 @@
+<?php
+// cabecera de la pagina
+include "includes/header.php";
+require_once "includes/utilidades.php";
+
+$search = $_POST['search_term'];
+
+if ($search) {
+    echo "<h1>Snippets with search term \"". $search ."\" on title or on tag</h1>";
+} else {
+    echo "<h1>Showing all the snippets</h1>";
+}
+
+// "% Matches any number of characters, even zero characters"
+// es como usar * en un regex
+$sql = "SELECT DISTINCT f_ult_mod, titulo, id FROM articulo
+        JOIN categorias ON articulo.id = categorias.id_articulo
+        WHERE categorias.categoria LIKE '%". $search ."%'
+        OR articulo.titulo LIKE '%". $search ."%'
+        ORDER BY f_ult_mod DESC
+        ";
+
+CreateSnippetTable($sql);
+
+// pie de pagina
+include "includes/footer.php";
+?>
