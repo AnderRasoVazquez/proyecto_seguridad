9e156f25d767657646f30543c8eaa7ad94913bfc
DavidPerezGomez <davidperezgo@gmail.com>
2017-11-01 12:46:42 +0100

    añadidas opciones al menu de profile

--- profile.php
+++ profile.php
@@ -5,16 +5,18 @@ include "includes/utilidades.php";
 
 if (!isset($_SESSION["currentUser"])) {
     // si no hay sesión iniciada redirigimos a index
-    header("Location: index.php");
+    header("Location: formulario_login.php");
     exit();
 }
 $full_name = getFullNameOf($_SESSION["currentUser"]);
 ?>
 
 <h1>Welcome <?=$full_name?></h1>
-<input type="button" class="btn" value="Log out" onclick="document.location.href='logout.php'">
+<input type="button" class="btn" value="Log out" onclick="document.location.href='logout.php'"><br>
+<input type="button" class="btn" value="Your snippets" onclick="document.location.href='snippets_by_author.php?author=<?=$_SESSION["currentUser"]?>'"><br>
+<input type="button" class="btn" value="Preferences" onclick="document.location.href='preferences.php'"><br>
 
 <?php
-// cabecera de la pagina
+// pie de pagina
 include "includes/footer.php";
 ?>
