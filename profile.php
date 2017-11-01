fe795cc5c20553a170cdae75492bb6b9169ae2b7
DavidPerezGomez <davidperezgo@gmail.com>
2017-11-01 11:56:17 +0100

    añadida bienvenida al perfil

--- profile.php
+++ profile.php
@@ -1,14 +1,17 @@
 <?php
 // cabecera de la pagina
 include "includes/header.php";
+include "includes/utilidades.php";
 
 if (!isset($_SESSION["currentUser"])) {
     // si no hay sesión iniciada redirigimos a index
     header("Location: index.php");
     exit();
 }
+$full_name = getFullNameOf($_SESSION["currentUser"]);
 ?>
 
+<h1>Welcome <?=$full_name?></h1>
 <input type="button" class="btn" value="Log out" onclick="document.location.href='logout.php'">
 
 <?php
