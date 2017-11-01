3c0bc0d7a8a30422bead21a51602beb12ecb59e8
DavidPerezGomez <davidperezgo@gmail.com>
2017-11-01 10:53:18 +0100

    profile redirige a index si no hay sesion iniciada

--- profile.php
+++ profile.php
@@ -1,6 +1,12 @@
 <?php
 // cabecera de la pagina
 include "includes/header.php";
+
+if (!isset($_SESSION["currentUser"])) {
+    // si no hay sesión iniciada redirigimos a index
+    header("Location: index.php");
+    exit();
+}
 ?>
 
 <input type="button" class="btn" value="Log out" onclick="document.location.href='logout.php'">
